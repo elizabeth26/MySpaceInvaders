d8b202ee9ea8764a432a9cd1c2e7e7467b2f08db
iut <iut@10.0.2.15>
2018-05-28 15:31:04 +0200

    commit

---
+++ src/main/java/outils/MissileException.java
@@ -0,0 +1,9 @@
+package outils;
+
+@SuppressWarnings("serial")
+public class MissileException extends RuntimeException {
+
+    public MissileException(String message) {
+	   super(message);
+    }
+}
---
+++ src/test/java/fr/unilim/iut/Space_Invaders/VaisseauTest.java
@@ -0,0 +1,46 @@
+package fr.unilim.iut.Space_Invaders;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+
+import fr.unilim.iut.Space_Invaders.SpaceInvaders;
+import outils.MissileException;
+
+import org.junit.Test;
+
+public class VaisseauTest {
+	
+	 private SpaceInvaders spaceinvaders;
+
+	    @Before
+	    public void initialisation() {
+		    spaceinvaders = new SpaceInvaders(15, 10);
+	    }
+
+    @Test
+    public void test_MissileBienTireDepuisVaisseau_VaisseauLongueurImpaireMissileLongueurImpaire() {
+
+	   spaceinvaders.positionnerUnNouveauVaisseau(new Dimension(7,2),new Position(5,9), 2);
+	   spaceinvaders.tirerUnMissile(new Dimension(3,2),2);
+
+      assertEquals("" + 
+      "...............\n" + 
+      "...............\n" +
+      "...............\n" + 
+      "...............\n" + 
+      "...............\n" + 
+      "...............\n" + 
+      ".......MMM.....\n" + 
+      ".......MMM.....\n" + 
+      ".....VVVVVVV...\n" + 
+      ".....VVVVVVV...\n" , spaceinvaders.recupererEspaceJeuDansChaineASCII());
+   }
+    
+    @Test(expected = MissileException.class)
+	public void test_LongueurMissileSuperieureALongueurVaisseau_UneExceptionEstLevee() throws Exception {
+		Vaisseau vaisseau = new Vaisseau(new Dimension(5,2),new Position(5,9), 1);
+		vaisseau.tirerUnMissile(new Dimension(7,2),1);
+	}
+    
+}
